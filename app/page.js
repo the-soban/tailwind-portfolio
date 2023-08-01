b94b2353356d1ea26411df6cf7c4ffb6d60178e6
the-soban <sobanshafeq123@gmail.com>
2023-08-01 20:50:48 +0500

    removed theme changer

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { HiMail } from "react-icons/hi";
 import { AiFillTwitterCircle, AiFillLinkedin, AiFillInstagram, AiFillGithub } from 'react-icons/ai';
 import { BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoReact, BiLogoBootstrap, BiLogoTailwindCss, BiLogoSass, BiLogoGit, BiLogoPhp, BiLogoRedux } from "react-icons/bi";
 import { SiMysql, SiFigma, SiAdobexd } from 'react-icons/si';
@@ -10,8 +10,6 @@ import web1 from '../public/web1.png';
 import web2 from '../public/web2.png';
 import web3 from '../public/web3.png';
 import web4 from '../public/web4.png';
-import web5 from '../public/web5.png';
-import web6 from '../public/web6.png';
 
 export default function Home() {
   return (
@@ -20,7 +18,7 @@ export default function Home() {
         <nav className='navbar py-10 mb-12 flex justify-between'>
           <h1 className='text-2xl font-REM font-black tracking-wide md:text-3xl md:tracking-normal'><a href='#'>Soban</a></h1>
           <ul className='flex items-center gap-6'>
-            <li className='cursor-pointer text-2xl'><BsFillMoonStarsFill/></li>
+            <li className='cursor-pointer text-2xl'><HiMail/></li>
             <li><a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-xl' href='#'>My Resume</a></li>
           </ul>
         </nav>
@@ -43,7 +41,7 @@ export default function Home() {
       <section className='mt-12'>
         <div>
           <h3 className='text-3xl font-Poppins font-bold py-1'>My Skillset</h3>
-          <p className='text-md font-Poppins py-2 leading-8 text-gray-700'>My journey into web development started as a hobby, something I did in my spare time. I used to design website UIs in 
+          <p className='text-md font-Poppins py-2 leading-8 text-gray-700 dark:text-white'>My journey into web development started as a hobby, something I did in my spare time. I used to design website UIs in 
           Figma and then try to build them as live and interactive sites with HTML and CSS. But, soon I realized I needed to learn and master the more advanced tools and technologies
           of web development such as <span className='text-teal-600'>Javascript, ReactJS, NextJS, Typescript, PHP, NodeJS, ExpressJS, MongoDB/MySQL</span> and many others. That is 
           when I started my dive into these tools and developed a passion for web development.</p>
@@ -83,8 +81,8 @@ export default function Home() {
             <h3 className="text-3xl font-Poppins font-bold py-1 ">Recent Work</h3>
             <p className="text-md font-Poppins py-2 leading-8 text-gray-800">Here's some <span className='text-teal-600'>websites and UI Designs</span> I have worked on recently: </p>
           </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 lg:w-96">
+          <div className="flex flex-col gap-10 py-10 md:flex-row md:flex-wrap lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
               <Image
                 className="rounded-xl object-cover mb-6"
                 width={"100%"}
@@ -93,8 +91,8 @@ export default function Home() {
                 src={web1}
                 alt='project'
               />
-              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl' href='#'>Github Repo</a>
-              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl' href='#'>Live Demo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl md:text-xs lg:text-base' href='#'>Github Repo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl md:text-xs lg:text-base' href='#'>Live Demo</a>
             </div>
             <div className="basis-1/3 flex-1">
               <Image
@@ -105,8 +103,8 @@ export default function Home() {
                 src={web2}
                 alt='project'
               />
-              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl' href='#'>Github Repo</a>
-              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl' href='#'>Live Demo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl md:text-xs lg:text-base' href='#'>Github Repo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl md:text-xs lg:text-base' href='#'>Live Demo</a>
             </div>
             <div className="basis-1/3 flex-1">
               <Image
@@ -117,8 +115,20 @@ export default function Home() {
                 src={web3}
                 alt='project'
               />
-              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl' href='#'>Github Repo</a>
-              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl' href='#'>Live Demo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl md:text-xs lg:text-base' href='#'>Github Repo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl md:text-xs lg:text-base' href='#'>Live Demo</a>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-xl object-cover mb-6"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web4}
+                alt='project'
+              />
+              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl md:text-xs lg:text-base' href='#'>Github Repo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl md:text-xs lg:text-base' href='#'>Live Demo</a>
             </div>
           </div>
         </section>
