c69832b8b23f7979529384606a12b0283c6f317e
the-soban <sobanshafeq123@gmail.com>
2023-08-01 19:00:58 +0500

    added contact section

--- app/page.js
+++ app/page.js
@@ -41,23 +41,23 @@ export default function Home() {
       </section>
 
       <section className='mt-12'>
-        <div className='font-Poppins'>
-          <h3 className='text-3xl font-bold py-1'>My Skillset</h3>
-          <p className='text-md py-2 leading-8 text-gray-700'>My journey into web development started as a hobby, something I did in my spare time. I used to design website UIs in 
+        <div>
+          <h3 className='text-3xl font-Poppins font-bold py-1'>My Skillset</h3>
+          <p className='text-md font-Poppins py-2 leading-8 text-gray-700'>My journey into web development started as a hobby, something I did in my spare time. I used to design website UIs in 
           Figma and then try to build them as live and interactive sites with HTML and CSS. But, soon I realized I needed to learn and master the more advanced tools and technologies
           of web development such as <span className='text-teal-600'>Javascript, ReactJS, NextJS, Typescript, PHP, NodeJS, ExpressJS, MongoDB/MySQL</span> and many others. That is 
           when I started my dive into these tools and developed a passion for web development.</p>
-          <p className='text-md py-2 leading-8 text-gray-800'>I also have a background in working as a freelance <span className='text-teal-600'>Graphic Designer</span>, which has 
+          <p className='text-md font-Poppins py-2 leading-8 text-gray-800'>I also have a background in working as a freelance <span className='text-teal-600'>Graphic Designer</span>, which has 
           given me the ability to pay close attention to detail and develop a creative mindset. </p>
         </div>
         <div>
           <div className='font-Poppins text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
             <Image src={Design} className='inline-block relative z-10' width='94' height='94' alt='design' />
             <Image src={Code} className='inline-block relative -ml-10 mt-2 z-0' width='94' height='94' alt='code' />
-            <h3 className="text-xl font-semibold pt-8 pb-2  ">
+            <h3 className="text-xl font-Poppins font-semibold pt-8 pb-2  ">
                 Web Design & Development
               </h3>
-              <p className="text-md py-2">
+              <p className="text-md font-Poppins py-2">
                 Designing a great website first and then building it up from scratch requires the knowledge and usage of of design principles and theories, expertise in web technologies 
                 and sharp attention to detail. Here's the <span className='text-teal-600'>tech stack (tools and technologies)</span> that I use to build websites and web apps: 
               </p>
@@ -80,82 +80,59 @@ export default function Home() {
 
       <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 ">Recent Work</h3>
-            <p className="text-md py-2 leading-8 text-gray-800">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 ">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
+            <h3 className="text-3xl font-Poppins font-bold py-1 ">Recent Work</h3>
+            <p className="text-md font-Poppins py-2 leading-8 text-gray-800">Here's some <span className='text-teal-600'>websites and UI Designs</span> I have worked on recently: </p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
+            <div className="basis-1/3 flex-1 lg:w-96">
               <Image
-                className="rounded-xl object-cover"
+                className="rounded-xl object-cover mb-6"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
                 src={web1}
                 alt='project'
               />
+              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl' href='#'>Github Repo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl' href='#'>Live Demo</a>
             </div>
             <div className="basis-1/3 flex-1">
               <Image
-                className="rounded-xl object-cover"
+                className="rounded-xl object-cover mb-6"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
                 src={web2}
                 alt='project'
               />
+              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl' href='#'>Github Repo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl' href='#'>Live Demo</a>
             </div>
             <div className="basis-1/3 flex-1">
               <Image
-                className="rounded-xl object-cover"
+                className="rounded-xl object-cover mb-6"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
                 src={web3}
                 alt='project'
               />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-xl object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web4}
-                alt='project'
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-xl object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web5}
-                alt='project'
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-xl object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web6}
-                alt='project'
-              />
+              <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl' href='#'>Github Repo</a>
+              <a className='font-Poppins text-white bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 mx-4 rounded-xl' href='#'>Live Demo</a>
             </div>
           </div>
+        </section>
+
+        <section className='py-10'>
+          <div>
+            <h3 className="text-3xl font-Poppins font-bold py-1 ">Contact Me</h3>
+            <p className="text-md font-Poppins mb-4 py-2 leading-8 text-gray-800">Enough about me and my work. Now, let's talk about <span className='text-teal-600'>your project</span>.</p>
+            <a className='font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 my-4 rounded-xl' href='#'>Send me an Email</a>
+          </div>
+        </section>
+
+        <section className='flex justify-center align-middle'>
+          <span className='text-xs font-Poppins text-gray-600 text-center mt-12 mb-4 mx-auto inline-block'>Copyright © Soban Shafiq - 2023</span>
         </section>
     </main>
   );
