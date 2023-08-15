a67efd1ad0aa7e7218e30e57393fa31df6c9648a
the-soban <sobanshafeq123@gmail.com>
2023-08-15 23:50:01 +0500

    changed viewports

--- app/page.js
+++ app/page.js
@@ -33,24 +33,24 @@ export default function Home() {
     return (
         <main className="bg-white dark:bg-slate-900 px-12 md:px-20 lg:px-40">
             <section className="min-h-screen">
-                <nav className="navbar py-10 mb-12 flex justify-between">
-                    <h1 className="text-2xl dark:text-white font-REM font-black tracking-wide md:text-3xl md:tracking-wide">
+                <nav className="navbar py-10 mb-12 flex justify-between align-middle">
+                    <h1 className="text-2xl dark:text-white font-REM font-black tracking-wide md:text-3xl md:tracking-wide sm:text-xl">
                         <a href="#">SOBAN</a>
                     </h1>
                     <ul className="flex items-center gap-6">
-                        <li className="cursor-pointer text-2xl">
+                        <li className="cursor-pointer text-2xl sm:text-lg">
                             <a href="#">
                                 <ThemeBtn />
                             </a>
                         </li>
-                        <li className="cursor-pointer text-2xl">
+                        <li className="cursor-pointer text-2xl sm:text-lg">
                             <a href="mailto:sobanshafeq123@gmail.com">
                                 <HiMail className="text-black dark:text-white" />
                             </a>
                         </li>
                         <li>
                             <a
-                                className="font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-xl"
+                                className="font-Poppins text-white bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-xl sm:text-sm"
                                 target="_blank"
                                 href="/resume.pdf"
                                 rel="noopener noreferrer"
